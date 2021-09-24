81c4a38eb30dd363dd70410457a205f91f481252
Doug Owings <doug@dougowings.net>
2021-09-24 02:36:22 -0700

    update start script and nginx config

--- files/start.sh
+++ files/start.sh
@@ -1,17 +1,88 @@
 #!/bin/bash
 
 set -e
+shopt -s expand_aliases
+
+alias git=/usr/bin/git
+alias named=/usr/sbin/named
+alias named-checkconf=/usr/sbin/named-checkconf
+alias nginx=/usr/sbin/nginx
+alias printf=/usr/bin/printf
+alias rndc=/usr/sbin/rndc
+alias service=/usr/sbin/service
+alias ssh-keyscan=/usr/bin/ssh-keyscan
+
+hr='------------------------------------------------------------'
+
+printh() {
+  printf '%s\n%s\n%s\n' "$hr" "$1" "$hr"
+}
+
+restart_nginx() {
+  local nginxconf
+  if [[ -z "$NGINX_CONF" ]] && [[ ! -z "$git_" ]] && [[ -e "$git_/nginx.conf" ]]; then
+    nginxconf="$git_/nginx.conf"
+  else
+    nginxconf="/etc/nginx/nginx.conf"
+  fi
+  if service nginx status ; then
+    printf '%s\n' 'Stopping nginx'
+    nginx -s stop || true
+  fi
+  printf '%s\n' 'Starting nginx'
+  printh "Using nginx config: $nginxconf"
+  nginx -c "$nginxconf"
+}
+
+reload_bind() {
+  printh "Reloading bind config"
+  rndc reload
+  printh "Reloaded bind config"
+}
+
+update_static() {
+  local www_="/var/www"
+  local fcommit="$www_/commit.txt" fconf="$www_/config.txt"
+  if [[ ! -z "$git_" ]]; then
+    git -C "$git_" log -1 2>&1 > "$fcommit" || true
+  fi
+  named-checkconf -p "$CONF" 2>&1 > "$fconf" || true
+}
+
+refresh_interval() {
+  if [[ -z "$git_" ]]; then
+    printh "WARN: Refresh not supported in non-git mode" >&2
+    return 0
+  fi
+  while true; do
+    sleep "$REFRESH_INTERVAL"
+    local output=`git -C "$git_" pull`
+    if [[ "$output" =~ 'Already up to date' ]]; then
+      continue
+    fi
+    reload_bind
+    restart_nginx
+    update_static
+  done
+}
 
 if [[ ! -z "$CONFIG_GIT" ]]; then
-  echo "CONFIG_GIT defined: $CONFIG_GIT"
-  git_=/etc/bind/git
+  printh "CONFIG_GIT defined: $CONFIG_GIT"
+  git_="/etc/bind/git"
 
   if [[ -z "$GIT_BRANCH" ]]; then
-    GIT_BRANCH=main
+    GIT_BRANCH="main"
   fi
 
   if [[ ! -e "$git_/.git" ]]; then
-    ssh-keyscan github.com >> ~/.ssh/known_hosts
+    if [[ "$CONFIG_GIT" =~ 'github.com' ]]; then
+      ssh-keyscan github.com >> ~/.ssh/known_hosts
+    fi
+    if [[ ! -z "$SSH_SCAN" ]]; then
+      for host in $SSH_SCAN ; do
+        ssh-keyscan "$host" >> ~/.ssh/known_hosts
+      done
+    fi
     git clone "$CONFIG_GIT" "$git_"
   else
     mkdir -pv "$git_"
@@ -21,9 +92,6 @@ if [[ ! -z "$CONFIG_GIT" ]]; then
   git -C "$git_" checkout "$GIT_BRANCH"
 
   conf="$git_/named.conf"
-  if [[ -z "$NGINX_CONF" ]] && [[ -e "$git_/nginx.conf" ]]; then
-    NGINX_CONF="$git_/nginx.conf"
-  fi
 else
   conf="/etc/bind/named.conf"
 fi
@@ -32,41 +100,19 @@ if [[ -z "$CONF" ]]; then
   CONF="$conf"
 fi
 
-if [[ -z "$NGINX_CONF" ]]; then
-  NGINX_CONF=/etc/nginx/nginx.conf
-fi
-
-echo "Using CONF=$CONF"
-echo "Using NGINX_CONF=$NGINX_CONF"
-
-shopt -s expand_aliases
-
-refresh_interval() {
-  if [[ -z "$git_" ]]; then
-    echo "WARN: Refresh not supported in non-git mode" >&2
-    return 0
-  fi
-  while true; do
-    sleep "$REFRESH_INTERVAL"
-    local output=`git -C "$git_" pull`
-    if [[ "$output" =~ 'Already up to date' ]]; then
-      continue
-    fi
-    rndc reload
-    nginx -s reload
-  done
-}
+printh "Using named config: $CONF"
 
 if [[ ! -z "$REFRESH_INTERVAL" ]]; then
   if [[ "$REFRESH_INTVERAL" -lt 60 ]]; then
     REFRESH_INTERVAL=60
   fi
-  echo "Using refresh interval of $REFRESH_INTERVAL seconds"
+  printh "Using refresh interval of $REFRESH_INTERVAL seconds"
 
   refresh_interval &
   refreshpid="$!"
 fi
 
-nginx -c "$NGINX_CONF"
-
-/usr/sbin/named -g -c "$CONF" -u bind
+restart_nginx
+update_static
+printh "Starting bind"
+named -g -c "$CONF" -u bind
