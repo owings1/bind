68699d658723b7e1b68370b08ed39133d482144c
Doug Owings <doug@dougowings.net>
2021-09-23 22:42:50 -0700

    refresh from git if enabled

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM store/internetsystemsconsortium/bind9:9.16
 
 RUN apt-get update -qq && apt-get -qqy upgrade && apt-get install -qqy \
-    curl nginx
+    curl nginx git && \
+    apt-get clean
+
+RUN apt-get update -qq && apt-get install -qqy --no-install-recommends \
+    nano && \
+    apt-get clean
 
 COPY files /docker
 
--- files/start.sh
+++ files/start.sh
@@ -1,3 +1,55 @@
 #!/bin/bash
+
+set -e
+
+if [[ ! -z "$CONFIG_GIT" ]]; then
+  echo "CONFIG_GIT defined: $CONFIG_GIT"
+  git_=/etc/bind/git
+
+  if [[ -z "$GIT_BRANCH" ]]; then
+    GIT_BRANCH=main
+  fi
+
+  if [[ ! -e "$git_/.git" ]]; then
+    ssh-keyscan github.com >> ~/.ssh/known_hosts
+    git clone "$CONFIG_GIT" "$git_"
+  else
+    mkdir -pv "$git_"
+    git -C "$git_" fetch origin
+  fi
+
+  git -C "$git_" checkout "$GIT_BRANCH"
+
+  conf="$git_/named.conf"
+else
+  conf="/etc/bind/named.conf"
+fi
+
+shopt -s expand_aliases
+
+refresh_interval() {
+  while true; do
+    sleep "$REFRESH_INTERVAL"
+    if [[ ! -z "$git_" ]]; then
+      local output=`git -C "$git_" pull`
+      if [[ "$output" =~ 'Already up to date' ]]; then
+        continue
+      fi
+    fi
+    rndc reload
+  done
+}
+
+if [[ ! -z "$REFRESH_INTERVAL" ]]; then
+  if [[ "$REFRESH_INTVERAL" -lt 60 ]]; then
+    REFRESH_INTERVAL=60
+  fi
+  echo "Using refresh interval of $REFRESH_INTERVAL seconds"
+
+  refresh_interval &
+  refreshpid="$!"
+fi
+
 service nginx start
-/usr/sbin/named -g -c /etc/bind/named.conf -u bind
+
+/usr/sbin/named -g -c "$conf" -u bind
